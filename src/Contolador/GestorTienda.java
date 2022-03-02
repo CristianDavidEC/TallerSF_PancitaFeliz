20a6a6218bb7b1cad22f5ab49ea923423ca9d7b5
CristianDavid <cristianescobarcdec@gmail.com>
2022-03-01 21:48:29 -0500

    archivos locales

--- src/Contolador/GestorTienda.java
+++ src/Contolador/GestorTienda.java
@@ -17,17 +17,24 @@ public class GestorTienda {
     public void cargarProductos (String path){
         ArrayList<String[]> datos = Archivo.leerArchivo(path);
         for (String[] dato: datos) {
-            System.out.println(dato[0]+dato[1]+dato[2]);
             Producto producto = new Producto(dato[0], dato[1], Double.parseDouble(dato[2]));
             this.productos.add(producto);
         }
     }
+
     public void cargarTiendas (String path){
         ArrayList<String[]> datos = Archivo.leerArchivo(path);
         for (String[] dato: datos) {
-            System.out.println(dato[0]+dato[1]);
             TiendaCliente tienda = new TiendaCliente(dato[0], dato[1]);
             this.tiendas.add(tienda);
         }
+    }
+
+    //------------ Gets ---------------------
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+    public ArrayList<TiendaCliente> getTiendas() {
+        return tiendas;
     }
 }
--- src/Frontera/Menu.java
+++ src/Frontera/Menu.java
@@ -1,11 +1,20 @@
 package Frontera;
 
 import Contolador.GestorTienda;
+import Entidades.Producto;
+import Entidades.TiendaCliente;
+import jdk.dynalink.beans.StaticClass;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Clase que se encarga de todos los dialogos e informacion que va a usar el usuario
+ */
 public class Menu {
-
+    /**
+     * Menu Principal de ejecucion del programa
+     */
     public static void menuInicial() {
         GestorTienda gestorTienda = new GestorTienda();
         gestorTienda.cargarProductos("src/Files/ProductosPancitaFeliz.txt");
@@ -14,25 +23,36 @@ public class Menu {
         //TODO: Inicializacion del objeto gestor tienda
         Scanner sc = new Scanner(System.in);
         int opcion;
-        do{
+        do {
             System.out.println("¡¡BIENVENIDO A PANCITA FELIZ!! \n");
             System.out.println("1.Ver Productos");
             System.out.println("2.Ver Tiendas");
             System.out.println("Elija Una Opcion");
             opcion = sc.nextInt();
 
-            switch (opcion){
+            switch (opcion) {
                 case 1:
-                    //TODO: Muestra los productos
+                    mostrarProductos(gestorTienda.getProductos());
                     break;
                 case 2:
-                    //TODO: Muestra las tiendas registradas
+                    mostrarTiendas(gestorTienda.getTiendas());
                     break;
                 case 0:
                     break;
                 default:
                     break;
             }
-        }while (opcion != 0);
+        } while (opcion != 0);
+    }
+
+    public static void mostrarProductos(ArrayList<Producto> productos){
+        for (Producto p: productos) {
+            System.out.println(p.getCodigoProducto() +" - "+ p.getNombreProducto() +" - "+ p.getValorUnitario());
+        }
+    }
+    public static void mostrarTiendas(ArrayList<TiendaCliente> tiendas){
+        for (TiendaCliente t: tiendas) {
+            System.out.println(t.getCodigoTienda() +" - "+ t.getNombreTienda());
+        }
     }
 }
