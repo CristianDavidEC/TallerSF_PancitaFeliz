61d0b0e8f257e0a0979633a1ca4b3442e61bb6cf
manue <manuela.1701813738@ucaldas.edu.co>
2022-03-03 21:46:49 -0500

    Valor total del pedido

--- src/Frontera/Menu.java
+++ src/Frontera/Menu.java
@@ -125,9 +125,9 @@ public class Menu {
                                 +"Nombre Producto: "+ p.getProducto().getNombreProducto() +"\n"
                                 +"Valor Unitario: "+ p.getProducto().getValorUnitario() +"\n"
                                 +"Cantidad Solicitada: "+ p.getCantidadPedida());
-            System.out.println("------------------------------------------");
-            System.out.println("Valor total: "+p.getValorTotal());
         }
+        System.out.println("------------------------------------------ \n");
+        System.out.println("Valor total del pedido: "+tienda.getOrden().calcularValor());
     }
 
     /*
